e155f1501c30c4272f904ff0a34d4b445f70f3a5
Lev Salmin <l.salmin@sxope.com>
2022-05-18 00:08:41 +0600

    feat(models): Create models for news migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function document(): HasMany
+    {
+        return $this->hasMany(News::class, 'user_id');
+    }
 }
